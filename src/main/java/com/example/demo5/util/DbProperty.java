9fa22f9fe22331462ff0ec2ab04c9abc60049149
e <again0912@163.com>
2020-06-09 07:43:45 +0800

    poi工具

---
+++ src/main/java/com/example/demo5/util/DbProperty.java
@@ -0,0 +1,22 @@
+package com.example.demo5.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbProperty {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            String driver = "com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/localbge?useUnicode=true&characterEncoding=utf8&allowMultiQueries=true&serverTimezone=GMT%2B8";
+            String username = "root";
+            String password = "root";
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
